ad235f4ee72ce2199e67a136186b8ab530c47e10
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-02-03 23:02:14 +0100

    "home bg"

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
 
   return (
         <>
-        <div className='text-white mt-20 mb-20 lg:mb-52 grid justify-start p-5 lg:justify-center lg:px-40'>
+        <div className='text-white mt-20 mb-20 lg:mb-52 grid justify-start p-5 lg:justify-center lg:px-40 bg-white'>
           <h1 data-aos = 'fade-down' className=' text-4xl lg:text-5xl font-nunito font-extrabold lg:mt-0'>{typedText}</h1>
           <div className='grid mt-1 lg:mt-8'>
           <p data-aos = 'fade-left' className='text-lg lg:text-2xl'>Daylight Insights Network is all about shining a spotlight on great talents and brands in Africa and beyond.</p>
