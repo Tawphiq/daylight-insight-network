7590eff55b41b0504a341d06591ec8b4237a41a6
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-02-04 17:24:09 +0100

    "browserrouter"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { HashRouterRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
