99e101ac114810caf57c28b578a854843de32f39
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-09 22:32:23 +0000

    "video upload"

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,6 +12,7 @@ import ReactPlayer from 'react-player';
 import pallazovid from '../assets/pallazovid.mp4';
 import latvid from '../assets/latvid.mp4';
 import allnicevid from '../assets/allnicevid.mp4';
+import dayl from '../assets/dayl.mp4';
 
 
 function Home() {
@@ -53,16 +54,19 @@ function Home() {
           <Events />
           <div className='md:grid md:grid-cols-2 md:gap-4 md:px-20'>
             <div>
-              <h3 className='p-4 mt-5 font-semibold text-lg'>Palazzo Salon Gh showing appreciation for being awarded and honored at West African Talents and Entrepreneurs Awards night </h3>
+              <h3 className='p-4 mt-5 font-semibold text-lg font-nunito'>Palazzo Salon Gh showing appreciation for being awarded and honored at West African Talents and Entrepreneurs Awards night </h3>
               <ReactPlayer url={pallazovid} controls={true} loop={false} width="100%" aspect-ratio="16/9" className="w-full h-48 mb-4 rounded" />
             </div>
             <div>
-            <h3 className='p-4 mt-5 font-semibold text-lg'>Congratulations to  @latifaadekanla ! 🎉 Celebrating their incredible talent and well-deserved victory!”</h3>
+            <h3 className='p-4 mt-5 font-semibold text-lg font-nunito'>Congratulations to  @latifaadekanla ! 🎉 Celebrating their incredible talent and well-deserved victory!”</h3>
               <ReactPlayer url={latvid} controls={true} loop={false} width="100%" aspect-ratio="16/9" className="w-full h-48 mb-4 rounded" />
             </div>
             <div>
-            <h3 className='p-4 mt-5 font-semibold text-lg'>Congratulations x 🎉 to ALLNICE CREATION. A video of appreciation for being awarded and honored at the awards night  as the  Best Makeup Artist of the year at West African Talents And Entrepreneurs Awards. Congratulations 🎉.</h3>
+            <h3 className='p-4 mt-5 font-semibold text-lg font-nunito'>Congratulations x 🎉 to ALLNICE CREATION. A video of appreciation for being awarded and honored at the awards night  as the  Best Makeup Artist of the year at West African Talents And Entrepreneurs Awards. Congratulations 🎉.</h3>
               <ReactPlayer url={allnicevid} controls={true} loop={false} width="100%" aspect-ratio="16/9" className="w-full h-48 mb-4 rounded" />
+            </div>
+            <div className='md:mt-40'>
+            <ReactPlayer url={dayl} controls={true} loop={false} width="100%" aspect-ratio="16/9" className="w-full h-48 mb-4 rounded" />
             </div>
           </div>
           <Contact />
